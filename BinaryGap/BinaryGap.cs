da85ab38351aa2b9da698e9498b46dc224588404
FeniXb3 <fenix.b3@gmail.com>
2019-07-27 19:41:31 +0200

    Add solution for BinaryGap submitted on Codility

---
+++ BinaryGap/BinaryGap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace BinaryGap
+{
+    class BinaryGap
+    {
+        public int Solution(int N)
+        {
+            var binary = Convert.ToString(N, 2);
+            // Func<int, int> map = (v, i) => new { Value = v, Index = i};
+
+            var result = binary.Select((v, i) => new {Value = Convert.ToBoolean(Char.GetNumericValue(v)), Index = i})
+                .Where(e => e.Value)
+                .Aggregate(new {Gap = 0, Index = 0}, (acc, v) =>
+                {
+                    var newGap = v.Index - acc.Index - 1;
+                    var gap = acc.Gap;
+                    if (newGap > gap)
+                    {
+                        gap = newGap;
+                    }
+
+                    return new {Gap = gap, Index = v.Index};
+                }, (r) => r.Gap);
+            return result;
+        }
+    }
+}
