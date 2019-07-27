6cc004769ba08ee7b0d13ea3fb7971e618121c99
FeniXb3 <fenix.b3@gmail.com>
2019-07-27 23:04:32 +0200

    Extract calculating gap to method

--- BinaryGap/BinaryGap.cs
+++ BinaryGap/BinaryGap.cs
@@ -13,18 +13,20 @@ namespace BinaryGap
             var result = binary.Select((v, i) => (Value: ToBoolean(v), Index: i))
                 .Where(e => e.Value)
                 .Select(v => v.Index)
-                .Aggregate((Gap: 0, Index: 0), (acc, newIndex) =>
-                {
-                    var (gap, oldIndex) = acc;
-                    var newGap = newIndex - oldIndex - 1;
-                    if (newGap > gap)
-                    {
-                        gap = newGap;
-                    }
-
-                    return (gap, newIndex);
-                }, r => r.Gap);
+                .Aggregate((Gap: 0, Index: 0), CalculateGap, r => r.Gap);
             return result;
+
+            (int, int) CalculateGap((int, int) acc, int newIndex)
+            {
+                var (gap, oldIndex) = acc;
+                var newGap = newIndex - oldIndex - 1;
+                if (newGap > gap)
+                {
+                    gap = newGap;
+                }
+
+                return (gap, newIndex);
+            }
 
             bool ToBoolean(char value)
             {
