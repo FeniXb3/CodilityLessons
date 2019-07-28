3e300afe4406447d51c87cbb76f0d409a3c11d5f
FeniXb3 <fenix.b3@gmail.com>
2019-07-28 14:18:25 +0200

    Add passing test with sample data

--- Lesson02.CyclicRotation.Tests/CyclicRotationTests.cs
+++ Lesson02.CyclicRotation.Tests/CyclicRotationTests.cs
@@ -1,9 +1,19 @@
-using System;
 using Xunit;
 
 namespace Lesson02.CyclicRotation.Tests
 {
     public class CyclicRotationTests
     {
+        [Fact]
+        void SampleDataTest()
+        {
+            var a = new[] {3, 8, 9, 7, 6};
+            var k = 3;
+
+            var solution = new CyclicRotationSolution();
+            var expected = new[] {9, 7, 6, 3, 8};
+
+            Assert.Equal(expected, solution.Solution(a, k));
+        }
     }
 }
--- Lesson02.CyclicRotation/CyclicRotationSolution.cs
+++ Lesson02.CyclicRotation/CyclicRotationSolution.cs
@@ -1,6 +1,18 @@
-﻿namespace Lesson02.CyclicRotation
+﻿using System.Linq;
+
+namespace Lesson02.CyclicRotation
 {
     public class CyclicRotationSolution
     {
+        public int[] Solution(int[] a, int k)
+        {
+            var amount = a.Length - k;
+            var tmp = a.Skip(amount);
+            var result = tmp
+                .Union(a.Take(amount))
+                .ToArray();
+
+            return result;
+        }
     }
 }
