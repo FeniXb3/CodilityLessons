d788995191d62c47cc135a60233eedbb5a797265
FeniXb3 <fenix.b3@gmail.com>
2019-07-28 14:30:09 +0200

    Optimize situation when cycles amount is equal to data length

--- Lesson02.CyclicRotation/CyclicRotationSolution.cs
+++ Lesson02.CyclicRotation/CyclicRotationSolution.cs
@@ -6,6 +6,9 @@ namespace Lesson02.CyclicRotation
     {
         public int[] Solution(int[] a, int k)
         {
+            if (k == a.Length)
+                return a;
+
             var amount = a.Length - k;
             var tmp = a.Skip(amount);
             var result = tmp
